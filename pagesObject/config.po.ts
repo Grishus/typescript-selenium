da38d9b879556c2ecba396da705d2c445b7cfbc5
Grisha <bobrik9494@gmail.com>
2019-12-09 21:37:23 +0300

    sdsdsd

--- pagesObject/config.po.ts
+++ pagesObject/config.po.ts
@@ -1,7 +1,7 @@
 export const App = {
     user: {
-        login: 'w@w.w',
-        password: 'w'
+        login: 'qwwqwe',
+        password: 'qwe'
     },
     url: 'http://dev2.lab-27.ru'
 }
--- tests/login/first.test.ts
+++ tests/login/first.test.ts
@@ -1,4 +1,4 @@
-import { Builder, WebDriver, Capabilities } from "selenium-webdriver";
+import { Builder, WebDriver, Capabilities, By } from "selenium-webdriver";
 import { LoginPage } from "../../pagesObject/login.po";
 import { CalendarPage } from "../../pagesObject//calendar.po";
 import { App } from "../../pagesObject/config.po";
@@ -14,7 +14,7 @@ const assert: IAssert = require("assert");
 let capabilities = Capabilities.chrome();
 
 capabilities.set("goog:chromeOptions", {
-  args: ["--lang=en", "disable-infobars", "--disable-plugins", "--headless"]
+  args: ["--lang=en", "disable-infobars", "--disable-plugins"]
 });
 
 describe("Login form", function() {
@@ -31,25 +31,36 @@ describe("Login form", function() {
   });
 
   it("Positive test", async function() {
-    browser.go(App.url);
-    await page.isLoad();
-    await browser.keys(page.email(), App.user.login);
-    await browser.keys(page.password(), App.user.password);
-    await browser.click(page.submit());
-    await calendarPage.isLoad();
-    await assert.equal(await calendarPage.isPage(), true);
-  });
+    // browser.go(App.url);
+    // await page.isLoad();
+    // await browser.keys(page.email(), App.user.login);
+    // await browser.keys(page.password(), App.user.password);
+    // await browser.click(page.submit());
+    driver.get('https://github.com/login');
 
-  it("Negative test", async function() {
-    debugger;
-    browser.go(App.url);
-    await page.isLoad();
-    await browser.keys(page.email(), App.user.login);
-    await browser.keys(page.password(), "qweqweqweqwe");
-    await browser.click(page.submit());
-    await page.isLoad();
-    await assert.equal(await page.isPage(), true);
-  });
+    await driver.sleep(4000);
+    await driver.findElement(By.css('[name="login"]')).sendKeys("Grishus");
+    await driver.sleep(2000);
+    await driver.findElement(By.css('[name="password"')).sendKeys("cjceyjr23");
+    await driver.findElement(By.css('[type="submit"]')).click();
+    await driver.sleep(1000);
+    await driver.findElement(By.css('data-ga-click="Dashboard, click, Sidebar header new repo button - context:user"')).click();
+    await driver.sleep(2000);
+    
+  //   await calendarPage.isLoad();
+  //   await assert.equal(await calendarPage.isPage(), true);
+   });
+
+  // it("Negative test", async function() {
+  //   debugger;
+  //   browser.go(App.url);
+  //   await page.isLoad();
+  //   await browser.keys(page.email(), App.user.login);
+  //   await browser.keys(page.password(), "qweqweqweqwe");
+  //   await browser.click(page.submit());
+  //   await page.isLoad();
+  //   await assert.equal(await page.isPage(), true);
+  // });
 
   after(() => driver && driver.quit());
 });
